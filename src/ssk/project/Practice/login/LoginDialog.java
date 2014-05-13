b2576dffc024348857467ee67d33ffd444815f26
Sophal Kim <sophal.kim.work@gmail.com>
2014-05-13 14:45:40 -0700

    They didn't handle the login to the reddit server

--- src/ssk/project/Practice/login/LoginDialog.java
+++ src/ssk/project/Practice/login/LoginDialog.java
@@ -3,6 +3,9 @@ package ssk.project.Practice.login;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.andrewshu.android.reddit.R;
@@ -32,6 +35,45 @@ public abstract class LoginDialog extends Dialog {
 			});
 		}
 		
+		loginUsernameInput = (EditText) findViewById(R.id.login_username_input);
+		loginUsernameInput.setOnKeyListener(new View.OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if ((event.getAction() == KeyEvent.ACTION_DOWN) 
+						&& (keyCode == KeyEvent.KEYCODE_ENTER || keyCode == KeyEvent.KEYCODE_TAB)) {
+					loginUsernameInput.requestFocus();
+					return true;
+				}
+				return false;
+			}
+		});
+		
+		loginPasswordInput = (EditText) findViewById(R.id.login_password_input);
+		loginPasswordInput.setOnKeyListener(new View.OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+					handleLoginChosen();
+					return true;
+				}
+				return false;
+			}
+		});
+		
+		final Button loginButton = (Button) findViewById(R.id.login_button);
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				handleLoginChosen();
+			}
+		});
+	}
+	
+	private void handleLoginChosen() {
+		String user = loginUsernameInput.getText().toString().trim();
+		String password = loginPasswordInput.getText().toString().trim();
+		onLoginChosen(user, password);
+	}
+	
+	private void onLoginChosen(String user, String password) {
 		
 	}
 
