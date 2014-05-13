b56e9a4f6743731dce2b2d53ec6ecb381b129e9d
Sophal Kim <sophal.kim.work@gmail.com>
2014-05-13 14:12:43 -0700

    Tiers of Kind

--- src/ssk/project/Practice/common/Constants.java
+++ src/ssk/project/Practice/common/Constants.java
@@ -20,4 +20,17 @@ public class Constants {
 	
 	public static final String COMMENT_PATH_PATTERN_STRING = "(?:/r/([^/]+)/comments|/comments|/tb)/([^/]+)(?:/?$|/[^/]+/([a-zA-Z0-9]+)?)?";
 	public static final String REDDIT_PATH_PATTERN_STRING = "(?:/r/([^/]+))?/?$";
+	public static final String USER_PATH_PATTERN_STRING = "/user/([^/]+)/?$";
+	
+	public static final String COMMENT_KIND = "t1";
+	public static final String THREAD_KIND = "t3";
+	public static final String MESSAGE_KIND = "t4";
+	public static final String SUBREDDIT_KIND = "t5";
+	public static final String MORE_KIND = "more";
+	
+	public static final int DEFAULT_THREAD_DOWNLOAD_LIMIT = 25;
+	public static final int DEFAULT_COMMENT_DOWNLOAD_LIMIT = 200;
+	public static final long DEFAULT_FRESH_DURATION = 18000000;
+	public static final long DEFAULT_FRESH_SUBREDDIT_LIST_DURATION = 86400000;
+	
 }
