e88b74e72f3b99dc7e6a5a608a624e4d1d35b8f2
Sophal Kim <sophal.kim.work@gmail.com>
2014-05-15 09:11:11 -0700

    XML arrays are nicely organized

--- src/ssk/project/Practice/settings/RedditPreferencesPage.java
+++ src/ssk/project/Practice/settings/RedditPreferencesPage.java
@@ -1,11 +1,14 @@
 package ssk.project.Practice.settings;
 
-import com.andrewshu.android.reddit.R;
-import com.andrewshu.android.reddit.common.Constants;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+import com.andrewshu.android.reddit.R;
+import com.andrewshu.android.reddit.common.Constants;
+import com.andrewshu.android.reddit.settings.RedditSettings;
 
 public class RedditPreferencesPage extends PreferenceActivity implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener{
 
@@ -56,6 +59,14 @@ public class RedditPreferencesPage extends PreferenceActivity implements Prefere
         	e.setEnabled(false);
         }
 	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		setRequestedOrientation(RedditSettings.Rotation.valueOf(prefs.getString(Constants.PREF_ROTATION, Constants.PREF_ROTATION_UNSPECIFIED)));
+	}
+	
 	@Override
 	public boolean onPreferenceClick(Preference preference) {
 		return false;
@@ -66,5 +77,8 @@ public class RedditPreferencesPage extends PreferenceActivity implements Prefere
 		return false;
 	}
 
-	
+	private CharSequence getVisualThemeName(String enumName) {
+		CharSequence[] visualNames = getResources().getTextArray(R.array.pref_theme_choices);
+		CharSequence[] enumNames = getResources().getTextArray(R.array.pref_theme_values);
+	}
 }
