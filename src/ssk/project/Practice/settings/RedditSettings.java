a1e156733757bbdfe938ecd8ddf2db6cae116b4a
Sophal Kim <sophal.kim.work@gmail.com>
2014-05-28 10:12:51 -0700

    SavePreferences() takes current info and save to SharedPreferences

--- src/ssk/project/Practice/settings/RedditSettings.java
+++ src/ssk/project/Practice/settings/RedditSettings.java
@@ -2,8 +2,11 @@ package ssk.project.Practice.settings;
 
 import org.apache.http.cookie.Cookie;
 
-import com.andrewshu.android.reddit.R;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
+import com.andrewshu.android.reddit.R;
 import com.andrewshu.android.reddit.common.Constants;
 
 public class RedditSettings {
@@ -58,6 +61,34 @@ public class RedditSettings {
 				return Constants.PREF_ROTATION_UNSPECIFIED;
 			}
 		}
+	}
+	
+	public void saveRedditPreferences(Context context) {
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+		SharedPreferences.Editor editor = settings.edit();
+		
+		if (username != null) {
+			editor.putString("username", username);
+		} else {
+			editor.remove("username");
+		}
+		if (redditSessionCookie != null) {
+			editor.putString("reddit_sessionValue", redditSessionCookie.getValue());
+			editor.putString("reddit_sessionDomain", redditSessionCookie.getDomain());
+			editor.putString("reddit_sessionPath", redditSessionCookie.getPath());
+			if (redditSessionCookie.getExpiryDate() != null) {
+				editor.putLong("reddit_sessionExpiryDate", redditSessionCookie.getExpiryDate().getTime());
+			}
+		}
+		if (modhash != null) {
+			editor.putString("modhash", modhash.toString());
+		}
+		editor.putString(Constants.PREF_HOMEPAGE, homepage.toString());
+		editor.putBoolean(Constants.PREF_USE_EXTERNAL_BROWSER, useExternalBrowser);
+		editor.putBoolean(Constants.PREF_CONFIRM_QUIT, confirmQuitOrLogout);
+		editor.putBoolean(Constants.PREF_SAVE_HISTORY, saveHistory);
+		editor.putBoolean(Constants.PREF_ALWAYS_SHOW_NEXT_PREVIOUS, alwaysShowNextPrevious);
+		editor.putString(Constants.PREF_COMMENTS_SORT_BY_URL, commentsSortByUrl);
 	}
 	
 }
