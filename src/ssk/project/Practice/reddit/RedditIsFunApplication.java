627c54b9c856504ffdf26951fd0d7a2971364513
Sophal Kim <sophal.kim.work@gmail.com>
2014-06-02 10:21:35 -0700

    They prefer to subclass Application instead of using a Singleton

---
+++ src/ssk/project/Practice/reddit/RedditIsFunApplication.java
@@ -0,0 +1,16 @@
+package ssk.project.Practice.reddit;
+
+import android.app.Application;
+
+public class RedditIsFunApplication extends Application {
+
+	private static RedditIsFunApplication application;
+	
+	public RedditIsFunApplication() {
+		application = this;
+	}
+	
+	public static RedditIsFunApplication getApplication() {
+		return application;
+	}
+}
