1f30c0c6e1877afd6594b1aab78a73b43b561b06
Sophal Kim <sophal.kim.work@gmail.com>
2014-06-21 15:37:15 -0700

    BroswerActivities uses an static block for compatibility

---
+++ src/ssk/project/Practice/reddit/browser/BrowserActivity.java
@@ -0,0 +1,41 @@
+package ssk.project.Practice.reddit.browser;
+
+import java.lang.reflect.Method;
+
+import ssk.project.Practice.settings.RedditSettings;
+import android.app.Activity;
+import android.net.Uri;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class BrowserActivity extends Activity {
+
+	private static final String TAG = "BrowserActivity";
+	
+	private WebView webview;
+	private Uri mUri = null;
+	private String mThreadUrl = null;
+	private String mTitle = null;
+	
+	private final RedditSettings mSettings = new RedditSettings();
+	
+	private static Method mWebSettings_setDomStorageEnabled;
+	private static Method mWebSettings_setLoadWithOverviewMode;
+	
+	static {
+		initCompatibility();
+	}
+	
+	private static void initCompatibility() {
+		try {
+			mWebSettings_setDomStorageEnabled = WebSettings.class.getMethod("setDomStorageEnabled", new Class[] { Boolean.TYPE });
+		} catch (NoSuchMethodException nsme) {
+			
+		}
+		try {
+			mWebSettings_setLoadWithOverviewMode = WebSettings.class.getMethod("setLoadWithOverviewMode", new Class[] { Boolean.TYPE });
+		} catch (NoSuchMethodException nsme) {
+			
+		}
+	}
+}
