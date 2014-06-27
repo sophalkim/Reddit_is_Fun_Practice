d18fd5f0afceb5de1ac89ede8276724189996876
Sophal Kim <sophal.kim.work@gmail.com>
2014-06-27 13:02:36 -0700

    Subclassing Exception class for custom ID

---
+++ src/ssk/project/Practice/reddit/captcha/CaptchaException.java
@@ -0,0 +1,10 @@
+package ssk.project.Practice.reddit.captcha;
+
+public class CaptchaException extends Exception {
+
+	static final long serialVersionUID = 40;
+	
+	public CaptchaException(String message) {
+		super(message);
+	}
+}
